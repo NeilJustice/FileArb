0539ac1f80db03253bbe1482d045eea0aa263362
Neil Justice <njjustice@gmail.com>
2020-11-21 22:20:06 -0600

    Remove unused file libFileArbTests/Components/FileSystem/MetalMock/LogFileMock.h

--- libFileArbTests/Components/FileSystem/MetalMock/LogFileMock.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "libFileArb/Components/FileSystem/LogFile.h"
-
-class LogFileMock : public Metal::Mock<LogFile>
-{
-public:
-   METALMOCK_VOID2(CreateIfDoesNotExistThenAppendLine, const fs::path&, string_view)
-   METALMOCK_VOID1_CONST(AppendLine, string_view)
-};

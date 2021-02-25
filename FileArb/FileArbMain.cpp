718cae5daeb10afb476549070aa9ce2395c4933c
Neil Justice <njjustice@gmail.com>
2021-02-25 16:38:13 -0600

    Add Boost Regex 1.75 include files and .lib files for Windows

--- FileArb/FileArbMain.cpp
+++ FileArb/FileArbMain.cpp
@@ -1,6 +1,14 @@
 #include "libFileArb/Compiler/IfMSVCIgnoreTheseWarningsGlobally.h"
 #include "libFileArb/Components/FileArbProgram.h"
 
+#if defined _WIN32
+#if defined _DEBUG
+#pragma comment(lib, "libboost_regex-vc142-mt-gd-x64-1_75.lib")
+#else
+#pragma comment(lib, "libboost_regex-vc142-mt-x64-1_75.lib")
+#endif
+#endif
+
 // create-binary-file --target=C:\FileArbTesting\Test --bytes=3 --random-bytes
 // create-binary-files --target=C:\FileRevisorTesting\Test --directories=1 --files=1 --bytes=3 --random-bytes
 // create-binary-files --target=C:\FileRevisorTesting\Test --directories=3 --files=1000 --bytes=3 --random-bytes --minimal

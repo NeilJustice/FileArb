af4d5bfdec8d4bcfb7200495a8a76b1c055fb42c
Neil Justice <njjustice@gmail.com>
2021-02-28 16:22:57 -0600

    Update variable name in RandomByteGenerator::NextRandomByte()

--- libFileArb/Components/Random/RandomByteGenerator.cpp
+++ libFileArb/Components/Random/RandomByteGenerator.cpp
@@ -5,8 +5,8 @@ char RandomByteGenerator::NextRandomByte() const
 {
    static random_device randomDevice;
    static default_random_engine defaultRandomEngine{ randomDevice() };
-   uniform_int_distribution<int> uniformLongLongDistribution(CHAR_MIN, CHAR_MAX);
-   const int nextRandomInt = uniformLongLongDistribution(defaultRandomEngine);
+   uniform_int_distribution<int> uniformIntDistribution(CHAR_MIN, CHAR_MAX);
+   const int nextRandomInt = uniformIntDistribution(defaultRandomEngine);
    const char nextRandomByte = static_cast<char>(nextRandomInt);
    return nextRandomByte;
 }

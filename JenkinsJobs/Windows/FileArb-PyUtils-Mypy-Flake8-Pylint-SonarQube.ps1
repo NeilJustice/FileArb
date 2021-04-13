0194eeeb21fed5527209ea0b687dadc60e2d4fa4
Neil Justice <njjustice@gmail.com>
2021-04-13 16:20:17 -0500

    Rename JenkinsJobs/Windows/FileArb-PyUtils-SonarQube.ps1 -> JenkinsJobs/Windows/FileArb-PyUtils-Mypy-Flake8-Pylint-SonarQube.ps1

--- JenkinsJobs/Windows/FileArb-PyUtils-Mypy-Flake8-Pylint-SonarQube.ps1
+++ JenkinsJobs/Windows/FileArb-PyUtils-Mypy-Flake8-Pylint-SonarQube.ps1
@@ -3,8 +3,8 @@ Set-PSDebug -Trace 1
 cd FileArbPyUtils
 $env:PYTHONPATH = '.'
 python.exe FileArbPyUtils\CoverageLintInstallPythonLibrary.py `
-	--project=FileArbPyUtils `
-	--run-tests-with-coverage-python-file=FileArbPyUtilsTests/RunAllWithCoverage.py
+  --project=FileArbPyUtils `
+  --run-tests-with-coverage-python-file=FileArbPyUtilsTests/RunAllWithCoverage.py
 cd ..
 
 Set-PSDebug -Trace 0

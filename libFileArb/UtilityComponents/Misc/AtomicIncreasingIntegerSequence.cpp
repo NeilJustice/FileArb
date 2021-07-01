83169fc91260661345ff0dafb19e0b822a8669a4
Neil Justice <njjustice@gmail.com>
2021-07-01 10:19:15 -0500

    Add explicitly defined empty default constructor to AtomicIncreasingIntegerSequence to possibly fix Codecov.io coverage issue

--- libFileArb/UtilityComponents/Misc/AtomicIncreasingIntegerSequence.cpp
+++ libFileArb/UtilityComponents/Misc/AtomicIncreasingIntegerSequence.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "libFileArb/UtilityComponents/Misc/AtomicIncreasingIntegerSequence.h"
 
+AtomicIncreasingIntegerSequence::AtomicIncreasingIntegerSequence()
+{
+}
+
 size_t AtomicIncreasingIntegerSequence::NextInteger()
 {
    const size_t nextIntegerInSequence = _atomicSizeT.fetch_add(1);
--- libFileArb/UtilityComponents/Misc/AtomicIncreasingIntegerSequence.h
+++ libFileArb/UtilityComponents/Misc/AtomicIncreasingIntegerSequence.h
@@ -5,6 +5,7 @@ class AtomicIncreasingIntegerSequence
 private:
    atomic<size_t> _atomicSizeT = 1;
 public:
+   AtomicIncreasingIntegerSequence();
    virtual ~AtomicIncreasingIntegerSequence() = default;
    virtual size_t NextInteger();
 };

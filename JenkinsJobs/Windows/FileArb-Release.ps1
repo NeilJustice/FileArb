0da5186a4c8a3e4dd69a2d96280fa2ff99fb99d7
Neil Justice <njjustice@gmail.com>
2023-07-30 21:56:15 -0500

    Remove JenkinsJobs/Windows/FileArb-Release.ps1

--- JenkinsJobs/Windows/FileArb-Release.ps1
+++
@@ -1,9 +0,0 @@
-$env:PYTHONPATH="."
-python.exe -u FileArbDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
-   --solution-name=FileArb `
-   --cmake-generator="Visual Studio 17 2022" `
-   --cmake-build-type=Release `
-   --tests-project-name=libFileArbTests `
-   --cmake-definitions="" `
-   --no-install
-exit $LastExitCode

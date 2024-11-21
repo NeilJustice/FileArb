bdb7087cb7a70813aa0c0a0c0bb9dc95d49cdd00
Neil Justice <njjustice@gmail.com>
2024-11-20 19:56:54 -0600

    Inline FileArbArgsEqualizerAndRandom.cpp into FileArbArgsEqualizerAndRandom.h and inline docoptValueRandom.cpp into docoptValueRandom.h

--- libFileArbTests/ValueTypes/ZenUnit/FileArbArgsEqualizerAndRandom.h
+++ libFileArbTests/ValueTypes/ZenUnit/FileArbArgsEqualizerAndRandom.h
@@ -7,10 +7,48 @@ namespace ZenUnit
    template<>
    struct Equalizer<FileArbArgs>
    {
-      static void AssertEqual(const FileArbArgs& expectedFileArbArgs, const FileArbArgs& actualFileArbArgs);
+      static void AssertEqual(const FileArbArgs& expectedFileArbArgs, const FileArbArgs& actualFileArbArgs)
+      {
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, commandLine);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, programMode);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, targetDirectoryPath);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, numberOfDirectoriesToCreate);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, numberOfFilesToCreate);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, numberOfLinesPerFile);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, numberOfCharactersPerLine);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, numberOfBytesPerFile);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, generateRandomBytes);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, generateRandomLetters);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, parallel);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, quiet);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, fileNamePrefix);
+         FIELDS_ARE_EQUAL(expectedFileArbArgs, actualFileArbArgs, fileExtension);
+      }
    };
 
-   FileArbArgs TestableFileArbArgs(const RandomGenerator* randomGenerator);
+   inline FileArbArgs TestableFileArbArgs(const RandomGenerator* randomGenerator)
+   {
+      FileArbArgs randomFileArbArgs;
+      randomFileArbArgs.commandLine = randomGenerator->String();
+      randomFileArbArgs.programMode = static_cast<ProgramMode>(randomGenerator->Enum(static_cast<int>(ProgramMode::MaxValue)));
+      randomFileArbArgs.targetDirectoryPath = randomGenerator->FilesystemPath();
+      randomFileArbArgs.numberOfDirectoriesToCreate = randomGenerator->SizeT();
+      randomFileArbArgs.numberOfFilesToCreate = randomGenerator->SizeT();
+      randomFileArbArgs.numberOfLinesPerFile = randomGenerator->SizeT();
+      randomFileArbArgs.numberOfCharactersPerLine = randomGenerator->SizeT();
+      randomFileArbArgs.numberOfBytesPerFile = randomGenerator->SizeT();
+      randomFileArbArgs.generateRandomBytes = randomGenerator->Bool();
+      randomFileArbArgs.generateRandomLetters = randomGenerator->Bool();
+      randomFileArbArgs.parallel = randomGenerator->Bool();
+      randomFileArbArgs.quiet = randomGenerator->Bool();
+      randomFileArbArgs.fileNamePrefix = randomGenerator->String();
+      randomFileArbArgs.fileExtension = randomGenerator->String();
+      return randomFileArbArgs;
+   }
+
    template<>
-   FileArbArgs Random();
+   inline FileArbArgs Random()
+   {
+      return TestableFileArbArgs(RandomGenerator::Instance());
+   }
 }
--- libFileArbTests/ValueTypes/ZenUnit/docoptValueRandom.h
+++ libFileArbTests/ValueTypes/ZenUnit/docoptValueRandom.h
@@ -4,5 +4,16 @@
 namespace ZenUnit
 {
    template<>
-   docopt::Value Random();
+   inline docopt::Value Random()
+   {
+      const long randomLong = ZenUnit::Random<long>();
+      if (randomLong % 2 == 0)
+      {
+         docopt::Value randomLongDocoptValue(randomLong);
+         return randomLongDocoptValue;
+      }
+      const string randomString = ZenUnit::Random<string>();
+      docopt::Value randomStringDocoptValue(randomString);
+      return randomStringDocoptValue;
+   }
 }

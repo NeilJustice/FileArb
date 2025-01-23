a50dce2844cb31032e3bad2c7be71c7403d0ff0e
Neil Justice <njjustice@gmail.com>
2025-01-23 07:37:42 -0600

    JenkinsJobs/Windows/FileArb-RelWithDebInfo.ps1: Update CMAKE_INSTALL_PREFIX from X:\ to C:\

--- JenkinsJobs/Windows/FileArb-RelWithDebInfo.ps1
+++ JenkinsJobs/Windows/FileArb-RelWithDebInfo.ps1
@@ -3,6 +3,6 @@ python.exe -u FileArbDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
    --solution-name=FileArb `
    --cmake-build-type=RelWithDebInfo `
    --tests-project-name=libFileArbTests `
-   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=X:\\" `
+   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=C:\\" `
    --install
 exit $LastExitCode

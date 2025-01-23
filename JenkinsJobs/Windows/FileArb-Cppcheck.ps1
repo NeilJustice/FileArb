d4b7778fcaee9c0c02cfcbb249ff88d05bb877f2
Neil Justice <njjustice@gmail.com>
2025-01-23 07:38:49 -0600

    JenkinsJobs/Windows/FileArb-Cppcheck.ps1: Update Cppcheck include from X:\include\ZenUnitAndMetalMock to C:\include\ZenUnitAndMetalMock

--- JenkinsJobs/Windows/FileArb-Cppcheck.ps1
+++ JenkinsJobs/Windows/FileArb-Cppcheck.ps1
@@ -25,7 +25,7 @@ cppcheck.exe `
    -DMETALMOCK_VOID4_CONST `
    -DRUN_TEMPLATE_TESTS `
    -I . `
-   -I X:\include\ZenUnitAndMetalMock `
+   -I C:\include\ZenUnitAndMetalMock `
    -I "C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um" `
    -I libFileArb `
    -j $numberOfLogicalProcessors `

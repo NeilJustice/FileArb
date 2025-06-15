04a23ec5d4a2296558ef12ed2455de66552fe5fc
Neil Justice <njjustice@gmail.com>
2025-06-15 17:25:16 -0500

    Fix JenkinsJobs/Linux/FileArb-DevOpsPython.sh

--- JenkinsJobs/Linux/FileArb-DevOpsPython.sh
+++ JenkinsJobs/Linux/FileArb-DevOpsPython.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
 
+cd FileArbDevOpsPython
 export PYTHONPATH=.
 python FileArbDevOpsPython/MypyFlake8PylintThenRunTestsWithCoverage.py --run-tests-with-coverage-python-file=FileArbDevOpsPythonTests/RunAllWithCoverage.py
+cd ..

a9011c49f75e194c333899805e2f7ca1c27adf2c
Neil Justice <njjustice@gmail.com>
2026-01-03 08:52:15 -0600

    Rename JenkinsJobs/Linux/FileArb-RelWithDebInfo-ClangUndefinedBehaviorSanitizer.sh -> JenkinsJobs/Linux/FileArb-Debug-Clang-UBSan.sh

--- JenkinsJobs/Linux/FileArb-Debug-Clang-UBSan.sh
+++ JenkinsJobs/Linux/FileArb-Debug-Clang-UBSan.sh
@@ -5,7 +5,7 @@ export CXX=clang++
 export PYTHONPATH=FileArbDevOpsPython
 python -u FileArbDevOpsPython/FileArbDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileArb \
-   --cmake-build-type=RelWithDebInfo \
+   --cmake-build-type=Debug \
    --tests-project-name=libFileArbTests \
    --cmake-definitions="-DClangUndefinedBehaviorSanitizerMode=ON" \
    --no-install

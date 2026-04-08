50111efcca446012ecd7aa0b7441a865323af011
Neil Justice <njjustice@gmail.com>
2026-04-08 07:35:27 -0500

    Add JenkinsJobs/Windows/FileArb-RelWithDebInfo-RunUnitTestsNTimes.ps1

---
+++ JenkinsJobs/Windows/FileArb-RelWithDebInfo-RunUnitTestsNTimes.ps1
@@ -0,0 +1,4 @@
+WindowsCPlusPlusBuilder.exe build-cpp-solution-then-run-unit-tests-n-times `
+   --solution-name=FileArb `
+   --tests-project-name=libFileArbTests `
+   --configuration=RelWithDebInfo

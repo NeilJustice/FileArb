77a1d17403718b2ef9ed59cf98ba2472ac33eec1
Neil Justice <njjustice@gmail.com>
2026-06-20 16:35:04 -0500

    Comment out JenkinsJobs/Linux/FileArb-Debug-Clang-IncludeWhatYouUse.sh

--- JenkinsJobs/Linux/FileArb-Debug-Clang-IncludeWhatYouUse.sh
+++ JenkinsJobs/Linux/FileArb-Debug-Clang-IncludeWhatYouUse.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
-set -ev
+#set -ev
 
-export CXX=clang++
-export PYTHONPATH=FileArbDevOpsPython
-python -u FileArbDevOpsPython/FileArbDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
-   --solution-name=FileArb \
-   --cmake-build-type=Debug \
-   --cmake-definitions="-DIncludeWhatYouUseMode=ON" \
-   --tests-project-name=libFileArbTests \
-   --no-install
+#export CXX=clang++
+#export PYTHONPATH=FileArbDevOpsPython
+#python -u FileArbDevOpsPython/FileArbDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
+#   --solution-name=FileArb \
+#   --cmake-build-type=Debug \
+#   --cmake-definitions="-DIncludeWhatYouUseMode=ON" \
+#   --tests-project-name=libFileArbTests \
+#   --no-install
+

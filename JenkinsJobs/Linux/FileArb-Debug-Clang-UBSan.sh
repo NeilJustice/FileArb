71c0a7dca36358c8114bb3faa114deb533bcffd8
Neil Justice <njjustice@gmail.com>
2026-07-05 18:27:44 -0500

    Add 'export UBSAN_OPTIONS=silence_unsigned_overflow=1' to JenkinsJobs/Linux/FileArb-Debug-Clang-UBSan.sh

--- JenkinsJobs/Linux/FileArb-Debug-Clang-UBSan.sh
+++ JenkinsJobs/Linux/FileArb-Debug-Clang-UBSan.sh
@@ -3,6 +3,7 @@ set -ev
 
 export CXX=clang++
 export PYTHONPATH=FileArbDevOpsPython
+export UBSAN_OPTIONS="silence_unsigned_overflow=1"
 python -u FileArbDevOpsPython/FileArbDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileArb \
    --cmake-build-type=Debug \

5ea5a478e7a1dacfdd02bfe9ebfb5455df9fc873
Neil Justice <njjustice@gmail.com>
2026-07-05 10:54:09 -0500

    Delete JenkinsJobs/Windows/FileArb-DevOpsPython.ps1

--- JenkinsJobs/Windows/FileArb-DevOpsPython.ps1
+++
@@ -1,4 +0,0 @@
-cd FileArbDevOpsPython
-$env:PYTHONPATH="."
-python.exe -u FileArbDevOpsPython\MypyFlake8PylintThenRunTestsWithCoverage.py --run-tests-with-coverage-python-file=FileArbDevOpsPythonTests/RunAllWithCoverage.py
-exit $LastExitCode

6d1034feddaff55e0830d352bb078c3b0c16a8ab
Neil Justice <njjustice@gmail.com>
2026-09-12 11:25:32 -0500

    Fix JenkinsJobs/Windows/FileArb-RelWithDebInfo-VSDiagnostics.ps1

--- JenkinsJobs/Windows/FileArb-RelWithDebInfo-VSDiagnostics.ps1
+++ JenkinsJobs/Windows/FileArb-RelWithDebInfo-VSDiagnostics.ps1
@@ -1,6 +1,7 @@
 AcceliTune.exe build-then-collect-vsdiagnostics `
    --working-dir="."  `
-   --program="FileArb" `
+   --solution=FileArb `
+   --program=FileArb `
    --configuration=RelWithDebInfo `
    --args-file="JenkinsJobs\Windows\VSDiagnosticsArgs.txt" `
    --session-id=61 `
